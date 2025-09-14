906b92760ec8c95f18e1dea2c470f8d36784f349
rajesh-nandy <136688430+rajesh-nandy@users.noreply.github.com>
2025-09-14 21:03:51 +0530

    Update particle.py

--- DE/particle.py
+++ DE/particle.py
@@ -1,35 +1,43 @@
 import numpy as np
+import cupy as cp
 
 class Particle:
-    def __init__(self, n_clusters, data, centroids_pos):
-        self.n_clusters = n_clusters
-        self.centroids_pos = centroids_pos
-        distances = self.get_distances(data=data)
-        clusters = np.argmin(distances, axis=0)
-        self.pb_val = self.fitness_function(clusters=clusters, distances=distances)
-        self.pb_clustering = clusters.copy()
-        
 
-    
-    def get_distances(self, data: np.ndarray):
+
+
+    def _get_distances(self, data: cp.ndarray):
             distances = []
             for centroid in self.centroids_pos:
-                d = np.linalg.norm(data - centroid, axis=1)
+                d = cp.linalg.norm(data - centroid, axis=1)
                 distances.append(d)
-            distances = np.array(distances)
-            
-                
-            return distances
-    
+            distances = cp.array(distances)
 
-    def fitness_function(self, clusters: np.ndarray, distances: np.ndarray) -> float:
+
+            return distances
+    def __init__(self, n_clusters, centroids_pos):
+        self.n_clusters = n_clusters
+        self.centroids_pos = cp.sort(centroids_pos)
+
+    def calculate_fitness(self, data: cp.ndarray):
+        distances = self._get_distances(data)
+        clusters = cp.argmin(distances, axis=0)
+        self.pb_val = self.fitness_function(clusters=clusters, distances=distances)
+
+    def calculate_cluster(self, data: cp.ndarray):
+        distances = self._get_distances(data)
+        clusters = cp.argmin(distances, axis=0)
+        self.pb_val = self.fitness_function(clusters=clusters, distances=distances)
+        self.pb_clustering = clusters.copy()
+
+    def fitness_function(self, clusters: cp.ndarray, distances: cp.ndarray) -> float:
         J = 0.0
         for i in range(self.n_clusters):
-            p = np.where(clusters == i)[0]
+            p = cp.where(clusters == i)[0]
             if len(p):
-                d = sum(distances[i][p])
+                dist = cp.array(distances[i][p])
+                d = cp.sum(dist)
                 d /= len(p)
                 J += d
         J /= self.n_clusters
+
         return J
-    
