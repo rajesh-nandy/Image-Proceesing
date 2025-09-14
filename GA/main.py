7332563a784b29b1fc850f8872403a914b2371a6
rajesh-nandy <136688430+rajesh-nandy@users.noreply.github.com>
2025-09-14 21:17:17 +0530

    Update main.py

--- GA/main.py
+++ GA/main.py
@@ -1,5 +1,6 @@
 import rasterio as rst
 from rasterio.plot import show
+import cupy as cp
 
 import cv2
 import numpy as np
@@ -10,15 +11,40 @@ img = rst.open("image samples/L3/l3f45q1408mar16/L3-NF45Q14-109-057-08Mar16-BAND
 
 
 image = img.read()
+#image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX, dtype = cv2.CV_8U)
 
 Z = image.reshape((-1,1))
 print(Z.shape)
 Z = np.float32(Z)
 
-ga = GAcluster(n_clusters= 5,n_particles=10, data=Z)
-res = ga.start(iteration=25)
+
+def cp_warmup():
+  a = cp.ones((1000,1000))
+  b = cp.ones((1000,1000))
+  c = cp.dot(a,b)
+cp_warmup()
+
+
+
+ga = None
+best_val = cp.inf
+best_res_old = None
+progress_old = None
+chk_old = []
+for _ in range(50):
+  ga = DEcluster(n_clusters= 5, n_particles=20, data=Z)
+  res, gb_val = ga.start(iteration=1000)
+  print(gb_val)
+  chk_old.append(gb_val)
+  if gb_val < best_val:
+    best_val = gb_val
+    best_res_old = res.copy()
+    progress_old = de.progress
+
+
 res2 = res.reshape((image.shape))
 show(res2)
+
 """cv2.imshow('res2',res2)
 cv2.waitKey(10000)
 cv2.destroyAllWindows()
